dc289f8c01ab66cd1c12d9c1e59460c5ea015f0f
Zephyrus2822 <rudranilchowdhury83@gmail.com>
2025-07-08 02:11:16 +0530

    dockerized

--- frontend/src/components/Landing/Landing.jsx
+++ frontend/src/components/Landing/Landing.jsx
@@ -132,7 +132,7 @@ const Landing = () => {
                     d="M17 8l4 4m0 0l-4 4m4-4H3"
                   />
                 </svg>
-                Get Started
+                Head to Blogs!
               </a>
             </div>
           </div>
